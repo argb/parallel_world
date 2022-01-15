a4f8e7cb18ae4f603673c5464371ee6bf8ae3d04
argb <argb@live.cn>
2022-01-15 19:47:14 +0800

    wg

--- chat/main/main.go
+++ chat/main/main.go
@@ -1,8 +1,23 @@
 package main
 
-import "parallel_world/chat/app"
+import (
+	"log"
+	"os"
+	"parallel_world/chat/app"
+)
 
 func main() {
+	f, err := os.OpenFile("/tmp/pworld.log", os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666)
+	defer f.Close()
+
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+
+	log.SetOutput(f)
+
+	log.Println("This is a test log entry")
+
 	myapp := app.NewApp()
 
 	myapp.Run()
